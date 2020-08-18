36192415be2a94f5fec488c94368165e1091949f
Marcin <marcin.rafal6@gmail.com>
2020-08-18 18:51:05 +0200

    Controller changes

--- src/main/java/com/example/demo/BasicController.java
+++ src/main/java/com/example/demo/BasicController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/RankBoard")
 public class BasicController {
 
     private final BoardGameRepo boardGameRepo;
@@ -24,8 +26,23 @@ public class BasicController {
         return (List<BoardGame>) boardGameRepo.findAll();
     }
 
+    @GetMapping("/games/{gameId}")
+    public Optional<BoardGame> getBoardGame(@PathVariable Long gameId) {
+        return boardGameRepo.findById(gameId);
+    }
+
+    @DeleteMapping("/games/{gameId}")
+    public void deleteGame(@PathVariable Long gameId){
+        boardGameRepo.deleteById(gameId);
+    }
+
     @PostMapping("/games")
-    void addGames(@RequestBody BoardGame boardGame){
+    void addGame(@RequestBody BoardGame boardGame){
+        boardGameRepo.save(boardGame);
+    }
+
+    @PutMapping("/games")
+    void editGame(@RequestBody BoardGame boardGame){
         boardGameRepo.save(boardGame);
     }
 }
